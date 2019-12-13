8e8a50136cb44c260280bafd7e963f590edd64f2
aaalmandre <aaalmandre@gmail.com>
2019-12-13 21:22:28 +0100

    solution 1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int sum = 0;
+            int massOfModule = 0;
+            int fuelReq = 0;
+            string line;
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"d:\Andris\c#\input.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                counter++;
+                massOfModule = Int32.Parse(line);
+                System.Console.WriteLine(massOfModule);
+
+                fuelReq = massOfModule / 3 - 2;
+                sum = sum + fuelReq;
+
+            }
+
+            file.Close();
+            System.Console.WriteLine("Fuel required to launch: {0}", sum);
+            // Suspend the screen.  
+            System.Console.ReadLine();
+
+        }
+    }
+}
