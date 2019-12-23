23e35910f8d28f0d36735d367ddb9e195646fe01
Almasi <andras.almasi@sap.com>
2019-12-23 14:30:00 +0100

    commit 3

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("asdfasdfa!");
+            Console.WriteLine("helloo!");
         }
     }
 }
