3e6a052be117b6fabbd49c5d2084901fb4b2a6fc
Almasi <andras.almasi@sap.com>
2019-12-31 16:02:12 +0100

    .

---
+++ ConsoleApplication3/lineSegment.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    static class intersectionTest
+    {
+        
+
+        public static bool isIntersecting(int x1, int y1, int x2, int y2)
+        {
+            return false;
+        }
+    }
+}
